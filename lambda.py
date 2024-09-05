0689ae790ec207b9339666a865c75f16b0343cf3
diegom3 <67805146+diegom3@users.noreply.github.com>
2024-09-05 11:29:28 -0400

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -5,13 +5,16 @@ sns_client = boto3.client('sns')
 sns_topic_arn = 'arn:aws:sns:<region>:<account-id>:<sns-topic-name>'  # Replace with your SNS topic ARN
 
 def lambda_handler(event, context):
-    # Parse the invokingEvent which is a string
-    invoking_event = json.loads(event['invokingEvent'])
+    # Parse the invokingEvent, which is a stringified JSON
+    invoking_event_str = event.get('invokingEvent', '{}')
     
-    # Extract the configuration item from the parsed event
-    if 'configurationItem' in invoking_event:
-        configuration_item = invoking_event['configurationItem']
-        
+    # Convert the stringified JSON into a dictionary
+    invoking_event = json.loads(invoking_event_str)
+    
+    # Check if 'configurationItem' exists in the parsed invokingEvent
+    configuration_item = invoking_event.get('configurationItem')
+    
+    if configuration_item:
         # Check if the resource type is FSx and the event is for creation
         if configuration_item['resourceType'] == 'AWS::FSx::FileSystem' and configuration_item['configurationItemStatus'] == 'ResourceDiscovered':
             # Send a notification to the SNS topic
